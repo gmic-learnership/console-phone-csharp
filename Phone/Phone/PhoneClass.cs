3a0048a80eff5a34e80c5a98e5d7c2ea4c5fb18a
FuniMaboho <funi.maboho@gmail.com>
2015-10-08 14:46:17 +0200

    PhoneApplicationFolder

---
+++ Phone/Phone/PhoneClass.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phone
+{
+    class PhoneClass
+    {
+        public string phone_number;
+
+        public string RemoveCharacters(string phone_number)
+        {
+            if (phone_number.Contains("(") || phone_number.Contains(")") || phone_number.Contains("-") || phone_number.Contains(" "))
+            {
+
+                phone_number = phone_number.Replace("(", "");
+                phone_number = phone_number.Replace(")", "");
+                phone_number = phone_number.Replace("-", "");
+                phone_number = phone_number.Replace(" ", "");
+
+            }
+
+            return phone_number;
+        }
+
+        public void CloseApp(string phone_num)
+        {
+            while (phone_num == "")
+            {
+                Console.WriteLine("Please enter a telephone number: ");
+                phone_num = Console.ReadLine();
+            }
+
+            while (phone_num == "exit")
+            {
+                Console.WriteLine("\n\t\tGoodbye :)");
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+            
+        }
+
+        public string checkFirst3Numbers(string phone_num)
+        {
+            string temp = phone_num.Substring(0, 3);
+
+
+            if (temp == "012" || temp == "011" || temp == "083" || temp == "072" || temp == "069" || temp == "073")
+            {
+
+                checkPhonebook(phone_num);
+            }
+            else
+            {
+                Console.WriteLine("\nSorry this number is invalid :( , please try to enter the number again\n");
+                Console.WriteLine("================================================\n");
+            }
+            return phone_num;
+        }
+
+        public string checkPhonebook(string phone_num)
+        {
+            string Funi = "0726070685";
+            string Tinyiko = "0737397153";
+            string Prudence = "0720751453";
+            string DVT = "0117595930";
+
+            if (phone_num.CompareTo(Funi) == 0)
+            {
+                Console.WriteLine("\nYour number is valid. Please hold while we dial");
+                Console.WriteLine("Dialing Funi ...\n...\n...");
+                Console.WriteLine("Successfully dialed Funi");
+            }
+
+            else if (phone_num.CompareTo(Tinyiko) == 0)
+            {
+                Console.WriteLine("\nYour number is valid. Please hold while we dial");
+                Console.WriteLine("Dialing Tinyiko ...\n...\n...");
+                Console.WriteLine("Successfully dialed Tinyiko");
+            }
+            else if (phone_num.CompareTo(DVT) == 0)
+            {
+                Console.WriteLine("\nYour number is valid. Please hold while we dial");
+                Console.WriteLine("Dialing DVT ...\n...\n...");
+                Console.WriteLine("Successfully dialed DVT");
+            }
+            else if (phone_num.CompareTo(Prudence) == 0)
+            {
+                Console.WriteLine("\nYour number is valid. Please hold while we dial");
+                Console.WriteLine("Dialing Prudence ...\n...\n...");
+                Console.WriteLine("Successfully dialed Prudence");
+            }
+            else
+
+            {
+                Console.WriteLine("\nYour number is valid. Please hold while we dial");
+                Console.WriteLine("Dialing " + phone_num + " ...\n...\n...");
+                Console.WriteLine("Successfully dialed " + phone_num);
+            }
+            return phone_num;
+        }
+
+        public void ValidateNumericValues()
+        {
+            try
+            {
+                int temp = Convert.ToInt32(phone_number);
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine("\nSorry, the number you entered is incorrect.");
+                Console.WriteLine("Please enter a valid, numeric telephone number\n", e);
+                Console.WriteLine("-------------------------------------------");
+                phone_number = Console.ReadLine();
+
+            }
+        }
+    }
+}
+
+
+
+
---
+++ Phone/Phone/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PhoneClass tel = new PhoneClass();
+            string phone_number;
+           
+            Console.WriteLine("\t\tWelcome to our telephone program");
+            Console.WriteLine("\t\t--------------------------------");
+            Console.WriteLine();
+
+            Console.WriteLine("Please enter a telephone number: ");
+            phone_number = Console.ReadLine();
+
+            tel.CloseApp(phone_number);
+
+            Console.WriteLine("\nThe phone number you entered is " + phone_number + "\n");
+
+            //this for loop iterates through the characters of the phone number then strips it of separation characters
+
+            do
+            {
+                for (int i = 1; i < phone_number.Length; i++)
+
+                {
+                   phone_number = tel.RemoveCharacters(phone_number);
+                }
+
+                tel.ValidateNumericValues();
+
+                //this loop checks if the number is avalid 10 digit number then calls a method to check 
+                // if its a number that falls in the list: 011, 012, 072, 083, 073, 069
+
+                if (phone_number.Length == 10)
+                {
+                   phone_number = tel.checkFirst3Numbers(phone_number);
+                }
+                else
+                {
+                    Console.WriteLine("\nSorry, you have entered an invalid number");
+                    Console.WriteLine("Please check your number and enter it again:(");
+                    Console.WriteLine("--------------------------------------------");
+                    
+                }
+                Console.WriteLine("\nThanx for using our telephone service\n\nType 'exit' to exit or enter another number:");
+                Console.WriteLine("================================================\n");
+                phone_number = Console.ReadLine();
+                tel.CloseApp(phone_number);
+            
+            } while (phone_number != "exit");
+        }
+    }
+}
